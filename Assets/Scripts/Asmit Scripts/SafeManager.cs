5da25007d6a2b38ebec285b259ad6fd52ee014d3
Asmit Singh Chauhan <blr.asmit@gmail.com>
2024-02-06 23:38:09 +0530

    safe sequence
    -added logic for safe sequence without audio

--- Assets/Scripts/Asmit Scripts/SafeManager.cs
+++ Assets/Scripts/Asmit Scripts/SafeManager.cs
@@ -9,11 +9,15 @@ public class SafeManager : MonoBehaviour, interactObject
     public charactermovement charMoverment;
     public GameObject cameraObj;
     public GameObject safeCamera;
-    public bool isEntry = true;
-    public int count = 0;
     public GameObject needlePivot;
+    public GameObject safe;
+    public GameObject playerObj;
+    // public bool isEntry = true;
+    public int count = 0;
+    public int hasBeenMoved = 0;
     public bool isInteracted;
-    private Quaternion rotation;
+    private Quaternion initialRotation;
+
     //private Quaternion initialRotation;
     public void Interact()
     {
@@ -25,19 +29,10 @@ public class SafeManager : MonoBehaviour, interactObject
             cameraObj.SetActive(false);
             safeCamera.SetActive(true);
             isInteracted = true;
-            isEntry = false;
+            // isEntry = false;
             ipod.hasBeenPressed = false;
-        }
-        else
-        {
-            Debug.Log("Again");
-            if (isEntry == false)
-            {
-                Debug.Log(" Setting Camera back");
-                charMoverment.speed = 4f;
-                cameraObj.SetActive(true);
-                safeCamera.SetActive(false); 
-            }
+            playerObj.GetComponent<MeshRenderer>().enabled = false;
+
         }
     }
     public bool ReInteract()
@@ -45,114 +40,85 @@ public class SafeManager : MonoBehaviour, interactObject
         return false;
     }
     
-    //to use esc to get back to the original camera and the player movement is unlocked again
-    public void EscapeKey()
+    //to use esc to get back to the original camera, the player movement is unlocked again, the puzzle is reset
+    private void EscapeKey()
     {
-        if (ipod.hasBeenPressed)
+        if (isInteracted)
         {
             if (Input. GetKeyDown(KeyCode.Escape))
             {
                 charMoverment.speed = 4f;
                 cameraObj.SetActive(true);
-                safeCamera.SetActive(false); 
+                safeCamera.SetActive(false);
+                hasBeenMoved = 0;
+                count = 0;
+                playerObj.GetComponent<MeshRenderer>().enabled = true;
+                isInteracted = false;
             }
         }
-    }
-
-    public void RotateRight()
-    { 
-        if(Input.GetKeyDown(KeyCode.D))
-        {
-            Debug.Log("Rotating Right");
-            //rotation = Quaternion.Euler(90, initialRotation.y + 90, 0);
-            needlePivot.transform.Rotate(0,0,-90);
-        }
-
-    }
-    public void RotateLeft()
-    { 
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log("Rotating Left");
-            //rotation = Quaternion.Euler(90, initialRotation.y + 90, 0);
-            needlePivot.transform.Rotate(0,0,90);
-        }
-
-    }
-
-    public void Solution()
+    } 
+    public void Solution()                             //checks the logic for the correct answer
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            if (count == 0)
+            if (count == 0 || count == 2 || count == 4)
             {
+                hasBeenMoved ++;
                 count++;
                 needlePivot.transform.Rotate(0, 0, 90);
             }
             else
             {
+                hasBeenMoved++;
                 needlePivot.transform.Rotate(0, 0, 90);
             }
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            if (count == 1)
+            if (count == 1 || count == 3)
             {
+                hasBeenMoved++;
                 count++;
                 needlePivot.transform.Rotate(0, 0, -90);
             }
             else
             {
+                hasBeenMoved++;
                 needlePivot.transform.Rotate(0, 0, -90);
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            if (count == 2)
-            {
-                count++;
-                needlePivot.transform.Rotate(0, 0, 90);
-            }
-            else
-            {
-                needlePivot.transform.Rotate(0, 0, 90);
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            if (count == 3)
-            {
-                count++;
-                needlePivot.transform.Rotate(0, 0, -90);
-            }
-            else
-            {
-                needlePivot.transform.Rotate(0, 0, -90);
-            }
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            if (count == 4)
-            {
-                count++;
-                needlePivot.transform.Rotate(0, 0, 90);
-            }
-            else
-            {
-                needlePivot.transform.Rotate(0, 0, 90);
             }
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialRotation = needlePivot.transform.rotation;
     }
-
     // Update is called once per frame
     void Update()
     {
         EscapeKey();
+        if (hasBeenMoved == 5 && count == 5)                    //checks for the correct answer
+        {
+            Debug.Log("Correct answer");
+            charMoverment.speed = 4f;
+            cameraObj.SetActive(true);
+            safeCamera.SetActive(false);
+            safe.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            needlePivot.SetActive(false);
+            playerObj.GetComponent<MeshRenderer>().enabled = true;
+        }
+        else if (hasBeenMoved == 5 && count != 5)               //checks for anything other than the correct answer
+        {
+            Debug.Log("Wrong answer");
+            charMoverment.speed = 4f;
+            cameraObj.SetActive(true);
+            safeCamera.SetActive(false);
+            hasBeenMoved = 0;
+            count = 0;
+            isInteracted = false;
+            playerObj.GetComponent<MeshRenderer>().enabled = true;
+            needlePivot.transform.rotation = initialRotation;
+        }
         if (isInteracted)
         { 
             Solution();
