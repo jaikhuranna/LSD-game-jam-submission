1cc75d4180e585cab4942e5efc0cc5d350ac5c4c
Asmit Singh Chauhan <blr.asmit@gmail.com>
2024-04-07 13:49:39 +0530

    Game over scene
    -make letter functionality

--- Assets/Scripts/Game Over Scripts/Button Manager.cs
+++ Assets/Scripts/Game Over Scripts/Button Manager.cs
@@ -7,16 +7,16 @@ using TMPro;
 public class ButtonManager : MonoBehaviour
 {
    // integers used to counter the number of clicks 
-   public int playagainCounter, mainmenuCounter, Counter;
+   public int playagainCounter, mainmenuCounter, Counter, backgroundButtonCounter;
    // Game objects used to set them Active 
    public GameObject playagainGameObj, mainmenuGameObj, gameoverObj, letterObj;
+   //ALL THE PARTS OF THE LETTER
+   public GameObject letterP1, letterP2, letterP3, letterP4;
+   //BUTTON TO TRIGGER THE LETTER
+   public GameObject backgroundButton;
    //You lost text 
    [SerializeField] private TMP_Text YLtext;
-
-   private void Start()
-   { 
-   }
-
+   
    public void playagainButton()
    {
       //adding counter when you press play again button
@@ -27,18 +27,18 @@ public class ButtonManager : MonoBehaviour
          playagainGameObj.SetActive(false);
       }
       // using switch case to change the position of the button UI
-      switch (playagainCounter)
-      {
-         case 1:
-            playagainGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-211,-21);
-            break;
-         case 2:
-            playagainGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(238,62);
-            break;
-         case 3:
-            playagainGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(292,-133);
-            break;
-      }
+      // switch (playagainCounter)
+      // {
+      //    case 1:
+      //       playagainGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-211,-21);
+      //       break;
+      //    case 2:
+      //       playagainGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(238,62);
+      //       break;
+      //    case 3:
+      //       playagainGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(292,-133);
+      //       break;
+      // }
 
    }
    
@@ -52,16 +52,36 @@ public class ButtonManager : MonoBehaviour
          mainmenuGameObj.SetActive(false);
       }
       // using switch case to change the position of the button UI
-      switch (mainmenuCounter)
+      // switch (mainmenuCounter)
+      // {
+      //    case 1:
+      //       mainmenuGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-282,-86);
+      //       break;
+      //    case 2:
+      //       mainmenuGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-160,97);
+      //       break;
+      //    case 3:
+      //       mainmenuGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(284,-92);
+      //       break;
+      // }
+   }
+
+   public void BackgroundButton()
+   {
+      backgroundButtonCounter++;
+      switch (backgroundButtonCounter)
       {
          case 1:
-            mainmenuGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-282,-86);
+            letterP1.SetActive(true);
             break;
          case 2:
-            mainmenuGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(-160,97);
+            letterP2.SetActive(true);
             break;
          case 3:
-            mainmenuGameObj.GetComponent<RectTransform>().anchoredPosition = new Vector2(284,-92);
+            letterP3.SetActive(true);
+            break;
+         case 4:
+            letterP4.SetActive(true);
             break;
       }
    }
@@ -72,7 +92,6 @@ public class ButtonManager : MonoBehaviour
       if (mainmenuCounter == Counter && playagainCounter == Counter)
       {
          gameoverObj.SetActive(false);
-         letterObj.SetActive(true);
       }
 
       switch (mainmenuCounter+playagainCounter)
@@ -98,10 +117,13 @@ public class ButtonManager : MonoBehaviour
             YLtext.rectTransform.anchoredPosition = new Vector2(-225, -127);
             break;
          case 6:
-            YLtext.text = "Dear Alan";
-            YLtext.rectTransform.anchoredPosition = new Vector2(-297, 195);
+            YLtext.transform.localScale = new Vector3(0.7f,.7f,.7f);
+            YLtext.transform.parent = letterObj.transform;
+            YLtext.text = "Dear Alan, ";
+            YLtext.rectTransform.anchoredPosition = new Vector2(-334, 176);
+            backgroundButton.SetActive(true);
             break;
-         
       }
+      
    }
 }
